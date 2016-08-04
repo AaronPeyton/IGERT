6b0e70d98aa3a2e74e39ecd4232e7cfaf9e0f183
Jared Fitton <jaredfitton@trinity.cs.ucsb.edu>
2016-08-04 16:39:42 -0700

    Added Politician_Network.py with votes calculated

---
+++ Politician_Network.py
@@ -0,0 +1,30 @@
+import csv
+
+data = list(csv.reader(open("Final_Inverted.csv")))
+
+sen_Names = []
+for row in data:
+	sen_Names.append(row[0])
+
+votes= [[0 for x in range(100)] for y in range(100)]
+
+for row in xrange(1,100):
+	for row2 in xrange(row+1,101):
+		for col in xrange(1,340):
+			if data[row][col] == data[row2][col]:
+				votes[row-1][row2-1] += 1
+				votes[row2-1][row-1] += 1
+			else:
+				votes[row-1][row2-1] -= 1
+				votes[row2-1][row-1] -= 1
+
+print votes
+"""
+temp = 0
+for col in xrange(1,340):
+	if data[1][col] == data[1][col]:
+		temp += 1
+	else:
+		temp -= 1
+print temp
+"""
