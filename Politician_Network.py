51d870fe33c937846be5b9e74e77369faced6bff
Jared Fitton <jaredfitton@trinity.cs.ucsb.edu>
2016-08-05 15:58:18 -0700

    Updated politician_Network.py

--- Politician_Network.py
+++ Politician_Network.py
@@ -1,15 +1,11 @@
 import csv
 import networkx as nx
-import matplotlib as plt
+import matplotlib.pyplot as plt
 #			Creating the adjacency matrix
 
 
 data = list(csv.reader(open("Final_Inverted.csv")))
 
-sen_Names = []
-for row in data:
-	sen_Names.append(row[0])
-#print sen_Names
 votes= [[0 for x in range(100)] for y in range(100)]
 
 for row in xrange(1,100):
@@ -35,5 +31,47 @@ print temp
 			
 
 #				Creating the Network graph
+
+sen_Names = []
+for row in data:
+	sen_Names.append('%s' %row[0])
+sen_Names = sen_Names[1:]
+print sen_Names
+
+g = nx.Graph()
+g.add_nodes_from(sen_Names)
+positions = nx.circular_layout(g)
+#print positions
+
+labels={}
+for name in sen_Names: 
+	labels[name] = r'%s' %name
+#print labels
+
+nx.draw_networkx_labels(g, positions, labels, font_size = 10)
+nx.draw(g, positions)
+
+
+plt.savefig("Politician_Graph.png")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
