9310dd4aa88fba38598bf8d762e2d1bfd043ccd8
Jared Fitton <jaredfitton@trinity.cs.ucsb.edu>
2016-08-08 14:23:54 -0700

    Updated

--- Politician_Network.py
+++ Politician_Network.py
@@ -14,20 +14,7 @@ for row in xrange(1,100):
 			if data[row][col] == data[row2][col]:
 				votes[row-1][row2-1] += 1
 				votes[row2-1][row-1] += 1
-			else:
-				votes[row-1][row2-1] -= 1
-				votes[row2-1][row-1] -= 1
-
 #print votes
-"""
-temp = 0
-for col in xrange(1,340):
-	if data[1][col] == data[1][col]:
-		temp += 1
-	else:
-		temp -= 1
-print temp
-"""
 			
 
 #				Creating the Network graph
@@ -36,19 +23,28 @@ sen_Names = []
 for row in data:
 	sen_Names.append('%s' %row[0])
 sen_Names = sen_Names[1:]
-print sen_Names
+#print sen_Names
 
 g = nx.Graph()
 g.add_nodes_from(sen_Names)
-positions = nx.circular_layout(g)
-#print positions
+temp = 0
+for row in xrange(100):
+	for col in xrange(100):
+		if votes[row][col] > 300:
+			g.add_edge(sen_Names[row], sen_Names[col], weight=votes[row][col])
+			temp += 1
+print temp
+
+
 
 labels={}
 for name in sen_Names: 
 	labels[name] = r'%s' %name
 #print labels
 
-nx.draw_networkx_labels(g, positions, labels, font_size = 10)
+
+positions = nx.spring_layout(g)
+nx.draw_networkx_labels(g, positions, labels, font_size = 7)
 nx.draw(g, positions)
 
 
